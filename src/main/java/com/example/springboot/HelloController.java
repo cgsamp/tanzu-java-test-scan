3dbd3ca2d46a01510b54e8ec972a64d54b57f85c
Christopher Samp <chrisgsamp@gmail.com>
2023-03-27 16:27:28 -0400

    Updated test

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Sampsoftware, Spring Boot + Tanzu, Shiva!";
+		return "Testing secure test and scan supply chain!";
 	}
 
 }
